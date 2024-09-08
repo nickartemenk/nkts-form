f0c46533694c26615d58dd9e185d7bcc036e6d1e
nickartemenk <artemenkokit@gmail.com>
2024-09-08 20:03:10 +0300

    add dataset

--- scripts/project-selection.js
+++ scripts/project-selection.js
@@ -1,19 +1,20 @@
 const formChecks = document.querySelectorAll('.form-check');
 
+let selectedProject = null;
+
 formChecks.forEach((elem) =>{
   elem.addEventListener(`click`, (e) => {
-    // console.log(`e`, e.target.tagName);
-    const parent = e.target.tagName === `DIV` ? e.target : e.target.parentElement || e.target.closest(`.form-check`);
-    console.log(parent);
+    const parent = e.target.tagName === `DIV` ? e.target : e.target.closest(`.form-check`);
 
     if (parent.classList.contains('active')) {
       parent.classList.remove('active');
+      selectedProject = null;
     } else {
       formChecks.forEach((item) => {
         item.classList.remove(`active`);
       })
-
       elem.classList.toggle('active');
+      selectedProject = elem.dataset.value;
     }
   });
 });
